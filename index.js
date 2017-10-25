d0cbf2c5c22a159c58291afc9da863ba895dd7ae
Andreas Madsen <amwebdk@gmail.com>
2017-10-25 11:02:34 +0200

    bubbleprof is not implemented

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 'use strict'
 
 class ClinicBubbleprof {
+  constructor (settings = {}) {
 
+  }
+
+  collect (args, callback) {
+    process.nextTick(callback, new Error('bubbleprof is not implemented'));
+  }
+
+  visualize (dataFilename, outputFilename, callback) {
+    process.nextTick(callback, new Error('bubbleprof is not implemented'));
+  }
 }
 
 module.exports = ClinicBubbleprof
