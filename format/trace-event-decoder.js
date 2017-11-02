a64c2e586173de9b8bc093ee971bc4a9d4874445
Andreas Madsen <amwebdk@gmail.com>
2017-11-02 12:03:01 +0100

    implement trace event decoder

---
+++ format/trace-event-decoder.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const stream = require('stream')
+const JSONStream = require('JSONStream')
+
+class TraceEvent {
+  constructor(data) {
+    const isCallback = data.name.slice(-'_CALLBACK'.length) === '_CALLBACK'
+
+    if (data.ph === 'b' && !isCallback) {
+      this.event = 'init'
+    } else if (data.ph === 'e' && !isCallback) {
+      this.event = 'destroy'
+    } else if (data.ph === 'b' && isCallback) {
+      this.event = 'before'
+    } else if (data.ph === 'e' && isCallback) {
+      this.event = 'after'
+    } else {
+      throw new Error('invalid phase: ' + data.ph)
+    }
+
+    this.type = isCallback ? data.name.slice(0, '_CALLBACK'.length) : data.name
+    this.asyncId = parseInt(data.id, 16)
+    this.timestamp = data.ts / 1000 // convert to ms
+    this.triggerId = null
+    if (data.args.hasOwnProperty('triggerId')) {
+      this.triggerId = data.args.triggerId
+    }
+  }
+}
+
+class TraceEventDecoder extends stream.Transform {
+  constructor() {
+    super({
+      readableObjectMode: true,
+      writableObjectMode: false
+    })
+
+    // JSONStream is synchronous so there is no need to think about
+    // backpresure
+    this.parser = JSONStream.parse('traceEvents.*')
+    this.parser.on('data', (data) => this.push(new TraceEvent(data)))
+  }
+
+  _transform(chunk, encoding, callback) {
+    this.parser.write(chunk, encoding)
+    callback(null)
+  }
+
+  _flush(callback) {
+    this.parser.end()
+    callback(null)
+  }
+}
+module.exports = TraceEventDecoder
