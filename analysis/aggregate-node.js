91d00ef34d320904dbbc7a7f9d1d2c7d8023c28a
Andreas Madsen <amwebdk@gmail.com>
2017-11-15 12:48:45 +0100

    remove redundant data in JSON

--- analysis/aggregate-node.js
+++ analysis/aggregate-node.js
@@ -10,6 +10,28 @@ class AggregateNode {
     this.sources = []
   }
 
+  toJSON() {
+    return {
+      nodeId: this.nodeId,
+      parentNodeId: this.parentNodeId,
+      children: this.children,
+      // frames and type are the same for all SourceNode's, so remove them
+      // from the SourceNode data.
+      frames: this.sources[0].frames,
+      type: this.sources[0].type,
+      sources: this.sources.map(function (source) {
+        return {
+          asyncId: source.asyncId,
+          triggerAsyncId: source.triggerAsyncId,
+          init: source.init,
+          before: source.before,
+          after: source.after,
+          destroy: source.destroy
+        }
+      })
+    }
+  }
+
   makeRoot () {
     this.addSourceNode(new SourceNode(1))
   }
