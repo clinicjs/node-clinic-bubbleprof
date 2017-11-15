25724fda544c1dafcbc601bfbec965658fd064bf
Andreas Madsen <amwebdk@gmail.com>
2017-11-15 15:53:24 +0100

    test source node filter

--- analysis/filter-source-nodes.js
+++ analysis/filter-source-nodes.js
@@ -10,6 +10,11 @@ class FilterSourceNodes extends stream.Transform {
   }
 
   _transform (node, encoding, callback) {
+    // * If a SourceNode doesn't have a stack trace it is because it has been
+    //   filtered in the `logger.js`.
+    // * TIMERWRAP are not really valuable to as they purely describe an
+    //   implementation detail in nodecore and never have any children as
+    //   triggerAsyncId.
     if (node.hasStackTrace() && node.type !== 'TIMERWRAP') this.push(node)
     callback(null)
   }
