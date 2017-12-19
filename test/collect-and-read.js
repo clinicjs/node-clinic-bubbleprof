b6f37f0414a5eba4a478c7f16e723fb098ce1494
Andreas Madsen <amwebdk@gmail.com>
2017-12-19 17:16:08 +0100

    be less intrusive, make tap --cov work

--- test/collect-and-read.js
+++ test/collect-and-read.js
@@ -1,12 +1,30 @@
 'use strict'
 
 const fs = require('fs')
+const http = require('http')
+const path = require('path')
 const async = require('async')
 const events = require('events')
+const endpoint = require('endpoint')
 const getLoggingPaths = require('../collect/get-logging-paths.js')
 const ClinicBubbleprof = require('../index.js')
 const StackTraceDecoder = require('../format/stack-trace-decoder.js')
 const TraceEventsDecoder = require('../format/trace-events-decoder.js')
+
+const testServerSockPath = path.resolve(__dirname, '.test-server.sock')
+
+function waitForFile (filepath, timeout, callback) {
+  fs.access(filepath, function (err) {
+    if (!err) return callback(null)
+    if (timeout <= 0) {
+      return callback(new Error('server did not listen within timeout'))
+    }
+
+    setTimeout(function () {
+      waitForFile(filepath, timeout - 50, callback)
+    }, Math.min(timeout, 50))
+  })
+}
 
 class CollectAndRead extends events.EventEmitter {
   constructor (...args) {
@@ -14,18 +32,34 @@ class CollectAndRead extends events.EventEmitter {
     const self = this
     const tool = new ClinicBubbleprof()
 
-    tool.collect([process.execPath, ...args], function (err, dirname) {
-      if (err) return self.emit('error', err)
+    fs.unlink(testServerSockPath, function (err) {
+      if (err && err.code !== 'ENOENT') return self.emit('error', err)
 
-      const files = getLoggingPaths(dirname.split('.')[0])
+      tool.collect([process.execPath, ...args], function (err, dirname) {
+        if (err) return self.emit('error', err)
 
-      const stacktrace = fs.createReadStream(files['/stacktrace'])
-        .pipe(new StackTraceDecoder())
-      const traceevents = fs.createReadStream(files['/traceevents'])
-        .pipe(new TraceEventsDecoder())
+        const files = getLoggingPaths(dirname.split('.')[0])
 
-      self._setupAutoCleanup(files, stacktrace, traceevents)
-      self.emit('ready', stacktrace, traceevents)
+        const stacktrace = fs.createReadStream(files['/stacktrace'])
+          .pipe(new StackTraceDecoder())
+        const traceevents = fs.createReadStream(files['/traceevents'])
+          .pipe(new TraceEventsDecoder())
+
+        self._setupAutoCleanup(files, stacktrace, traceevents)
+        self.emit('ready', stacktrace, traceevents)
+      })
+    })
+  }
+
+  request (href, callback) {
+    waitForFile(testServerSockPath, 1000, function (err) {
+      if (err) return callback(err)
+      http.get({
+        socketPath: testServerSockPath,
+        path: href
+      }, function (res) {
+        res.pipe(endpoint(callback))
+      })
     })
   }
 
