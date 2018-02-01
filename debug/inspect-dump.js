aa82bc2e762c5b866a917eb0739a5ee183898335
Andreas Madsen <amwebdk@gmail.com>
2018-02-01 15:16:33 +0100

    update dump debug scripts

--- debug/inspect-dump.js
+++ debug/inspect-dump.js
@@ -1,19 +1,22 @@
 'use strict'
 const fs = require('fs')
+const inspectpoint = require('inspectpoint')
+const analysis = require('../analysis/index.js')
 const getLoggingPaths = require('../collect/get-logging-paths.js')
+const SystemInfoDecoder = require('../format/system-info-decoder.js')
 const StackTraceDecoder = require('../format/stack-trace-decoder.js')
 const TraceEventDecoder = require('../format/trace-event-decoder.js')
-const analysis = require('../analysis/index.js')
-const AggregateNodesToDprof = require('./aggregate-nodes-to-dprof.js')
 
 // Load data
 const paths = getLoggingPaths({ path: process.argv[2] })
+const systemInfoReader = fs.createReadStream(paths['/systeminfo'])
+  .pipe(new SystemInfoDecoder())
 const stackTraceReader = fs.createReadStream(paths['/stacktrace'])
   .pipe(new StackTraceDecoder())
 const traceEventReader = fs.createReadStream(paths['/traceevent'])
   .pipe(new TraceEventDecoder())
 
-// create dataFile
-analysis(stackTraceReader, traceEventReader)
-  .pipe(new AggregateNodesToDprof())
+// Print data
+analysis(systemInfoReader, stackTraceReader, traceEventReader)
+  .pipe(inspectpoint({ depth: null, colors: true }))
   .pipe(process.stdout)
