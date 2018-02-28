d3cd538413c2161a6eea4d2fb107bdf0ab46b151
Mathias Buus <mathiasbuus@gmail.com>
2018-02-28 13:00:15 +0100

    add visualize-all helper

---
+++ debug/visualize-all.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const Tool = require('../')
+
+for (const file of process.argv.slice(2).map(trim)) {
+  const tool = new Tool()
+
+  tool.visualize(
+    file,
+    file + '.html',
+    function (err) {
+      if (err) {
+        console.error(file, 'failed (' + err.message + ')')
+      } else {
+        console.log('Wrote', file + '.html')
+      }
+    }
+  )
+}
+
+function trim (file) {
+  return file.replace(/\/\\$/, '')
+}
