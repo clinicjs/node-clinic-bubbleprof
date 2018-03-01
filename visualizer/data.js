10f8fd6e16dac215536c8e022acc7f5b1c9f4223
Alan Slater <alan.slater@nearform.com>
2018-03-01 17:56:56 +0000

    Add callbackEvents to sourceNodes

--- visualizer/data.js
+++ visualizer/data.js
@@ -109,5 +109,29 @@ class SourceNode {
     this.before = source.before
     this.after = source.after
     this.destroy = source.destroy
+
+    this.callbackEvents = source.before.map((value, callKey) => new CallbackEvent(source, callKey))
+  }
+}
+
+// The callback functions represented by a sourceNode's unique async_id
+// may be called any number of times. To calculate delays and busy time
+// we need to look at each call to these callbacks, relative to its source
+class CallbackEvent {
+  constructor (source, callKey) {
+    // Timestamp when this became the next call to this callback
+    this.delayStart = callKey === 0 ? source.init : source.after[callKey - 1]
+
+    // Timestamp when this callback call begins
+    this.before = source.before[callKey]
+
+    // Timestamp when this callback call completes
+    this.after = source.after[callKey]
+
+    this.syncTime = this.after - this.before
+    this.rawDelay = this.before - this.delayStart
+    this.adjustedDelays = new Map()
+   
+    this.source = source
   }
 }
