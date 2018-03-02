33522ad9dffd98457b880eb0db3a5a66a0aef644
Alan Slater <alan.slater@nearform.com>
2018-03-02 15:25:34 +0000

    Review changes 2

--- visualizer/layout/line-coordinates.js
+++ visualizer/layout/line-coordinates.js
@@ -73,6 +73,7 @@ class LineCoordinates {
       y2: y1 + length * Math.sin(radians)
     }
   }
+
   static lineLengthFromXY ({x1, y1, x2, y2}) {
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
   }
