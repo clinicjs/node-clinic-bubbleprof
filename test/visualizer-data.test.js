e22d580e44457e91ac0358445ad98288e67c2f3e
Mathias Buus <mathiasbuus@gmail.com>
2018-03-05 12:36:19 +0100

    Reduce the amount of test assertions for easier debugging (#41)
    * reduce the amount of test assertions for easier debugging
    
    * Give feedback on point of failure if a test fails

--- test/visualizer-data.test.js
+++ test/visualizer-data.test.js
@@ -6,23 +6,24 @@ const slowioJson = require('./visualizer-util/sampledata-slowio.json')
 const acmeairJson = require('./visualizer-util/sampledata-acmeair.json')
 const fakeJson = require('./visualizer-util/fakedata.json')
 
-function validateData (data, t) {
+function validateData (data) {
   for (const [clusterId, clusterNode] of data) {
-    t.ok(clusterNode.name)
-    t.ok(clusterId > clusterNode.parentClusterId)
+    if (!clusterNode.name) return `1: fails on clusterId ${clusterId}`
+    if (clusterId <= clusterNode.parentClusterId) return `2: fails on clusterId ${clusterId}`
 
     for (const aggregateNode of clusterNode.nodes) {
-      t.ok(aggregateNode.mark.get(0))
-      t.ok(aggregateNode.aggregateId > aggregateNode.parentAggregateId)
-      if (!aggregateNode.isRoot) t.ok(aggregateNode.type)
+      if (!aggregateNode.mark.get(0)) return `3:  fails on aggregateId ${aggregateNode.aggregateId}`
+      if (aggregateNode.aggregateId <= aggregateNode.parentAggregateId) return `4: fails on aggregateId ${aggregateNode.aggregateId}`
+      if (!aggregateNode.isRoot && !aggregateNode.type) return `5: fails on aggregateId ${aggregateNode.aggregateId}`
 
       for (const sourceNode of aggregateNode.sources) {
-        t.ok(sourceNode.asyncId)
-        t.equals(sourceNode.after.length, sourceNode.callbackEvents.length)
-        t.ok(sourceNode.asyncId > sourceNode.parentAsyncId)
+        if (!sourceNode.asyncId) return `6: fails with no sourceNode id, aggregateId ${aggregateNode.aggregateId}`
+        if (sourceNode.after.length !== sourceNode.callbackEvents.length) return `7:  fails on asyncId ${sourceNode.asyncId}`
+        if (sourceNode.asyncId <= sourceNode.parentAsyncId) return `8: fails on asyncId ${sourceNode.asyncId}`
       }
     }
   }
+  return 'Pass'
 }
 
 test('Visualizer data - examples/slow-io sample json', function (t) {
@@ -30,7 +31,7 @@ test('Visualizer data - examples/slow-io sample json', function (t) {
     t.ifError(err)
 
     t.equals(data.size, 33)
-    validateData(data, t)
+    t.equals(validateData(data), 'Pass')
 
     t.end()
   }, slowioJson)
@@ -41,7 +42,7 @@ test('Visualizer data - acmeair sample json', function (t) {
     t.ifError(err)
 
     t.equals(data.size, 24)
-    validateData(data, t)
+    t.equals(validateData(data), 'Pass')
 
     t.end()
   }, acmeairJson)
