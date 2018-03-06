fe639249766a153c585155e98ce5f25a09d03898
Alan Slater <alan.slater@nearform.com>
2018-03-06 11:31:35 +0000

    Restructure data logic, scope allcallbacks array

--- visualizer/data/data-node.js
+++ visualizer/data/data-node.js
@@ -1,27 +1,45 @@
 'use strict'
 
-const data = require('./data.json') // base64 encoded source file
+const CallbackEvent = require('./callback-event.js')
 
-// 'data = data' optional arg allows json to be passed in for browserless tests
-function loadData (callback, json = data) {
-  setTimeout(function () {
-    callback(null, wrapData(json))
-  })
+function isNumber (num) {
+  return typeof num === 'number' && !Number.isNaN(num)
 }
-module.exports = loadData
 
-function wrapData (data) {
+function wrapData (data, settings) {
   if (!data.map) {
-    console.warn('No valid data found, data.json contains', typeof data, data)
-    return new Map()
+    throw new Error(`No valid data found, data.json is ${typeof data}`, data)
   }
-  return new Map(
-    data.map((node) => [node.clusterId, new ClusterNode(node)])
+
+  const defaultSettings = {
+    averaging: 'mean', // across callbackEvents. Alternatives: 'median', 'sum'
+    quantileRange: 99, // set null to keep all outliers
+    idleOnly: false // if true, discounts async delays while sync process blocks event loop
+  }
+
+  settings = Object.assign(defaultSettings, settings)
+
+  const dataWrap = new Map(
+    data.map((node) => [node.clusterId, new ClusterNode(node, settings)])
   )
+  CallbackEvent.identifyOverlaps()
+
+  dataWrap.settings = settings
+
+  return dataWrap
 }
 
-class ClusterNode {
-  constructor (node) {
+class DataNode {
+  constructor (settings) {
+    // Reference to settings on data map
+    this.settings = settings
+
+  }
+}
+
+class ClusterNode extends DataNode {
+  constructor (node, settings) {
+    super(settings)
     this.isRoot = (node.clusterId === 1)
 
     this.clusterId = node.clusterId
@@ -30,7 +48,9 @@ class ClusterNode {
 
     this.children = node.children
     this.nodes = node.nodes
-      .map((aggregateNode) => new AggregateNode(aggregateNode))
+      .map((aggregateNode) => new AggregateNode(aggregateNode, settings))
+
+    console.log(`clusterId ${this.clusterId}'s stats are within: ${this.getStat('within')} between: ${this.getStat('between')}`)
   }
 }
 
@@ -44,8 +64,9 @@ class Mark {
   }
 }
 
-class AggregateNode {
-  constructor (node) {
+class AggregateNode extends DataNode {
+  constructor (node, settings) {
+    super(settings)
     this.isRoot = (node.aggregateId === 1)
 
     this.aggregateId = node.aggregateId
@@ -62,7 +83,7 @@ class AggregateNode {
         data: frameItem
       }
     })
-    this.sources = node.sources.map((source) => new SourceNode(source))
+    this.sources = node.sources.map((source) => new SourceNode(source, settings))
   }
 }
 
@@ -109,8 +130,10 @@ class Frame {
   }
 }
 
-class SourceNode {
-  constructor (source) {
+class SourceNode extends DataNode {
+  constructor (source, settings) {
+    super(settings)
+
     this.asyncId = source.asyncId
     this.parentAsyncId = source.parentAsyncId
     this.triggerAsyncId = source.parentAsyncId
@@ -125,24 +148,9 @@ class SourceNode {
   }
 }
 
-// The callback functions represented by a sourceNode's unique async_id
-// may be called any number of times. To calculate delays and busy time
-// we need to look at each call to these callbacks, relative to its source
-class CallbackEvent {
-  constructor (source, callKey) {
-    // Timestamp when this became the next call to this callback
-    this.delayStart = callKey === 0 ? source.init : source.after[callKey - 1]
-
-    // Timestamp when this callback call begins
-    this.before = source.before[callKey]
-
-    // Timestamp when this callback call completes
-    this.after = source.after[callKey]
-
-    this.syncTime = this.after - this.before
-    this.rawDelay = this.before - this.delayStart
-    this.adjustedDelays = new Map()
-
-    this.source = source
-  }
+module.exports = {
+  wrapData,
+  ClusterNode,
+  AggregateNode,
+  SourceNode
 }
--- visualizer/data/index.js
+++ visualizer/data/index.js
@@ -2,31 +2,13 @@
 
 const data = require('../data.json') // base64 encoded source file
 
+const wrapData = require('./data-node.js').wrapData
+
 // 'json = data' optional arg allows json to be passed in for browserless tests
 function loadData (callback, json = data, settings = {}) {
   setTimeout(function () {
-    const data = wrapData(json)
-
-    const defaultSettings = {
-      averaging: 'mean', // across callbackEvents. Alternatives: 'median', 'sum'
-      quantileRange: 99, // set null to keep all outliers
-      idleOnly: false // set true to only count delays when no sync in progress
-    }
-
-    settings = Object.assign(defaultSettings, settings)
-    data.settings = settings
-
+    const data = wrapData(json, settings)
     callback(null, data)
   })
 }
 module.exports = loadData
-
-function wrapData (data, settings) {
-  if (!data.map) {
-    console.warn('No valid data found, data.json contains', typeof data, data)
-    return new Map()
-  }
-  return new Map(
-    data.map((node) => [node.clusterId, new ClusterNode(node, settings)])
-  )
-}
