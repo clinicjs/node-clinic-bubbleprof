1b25b9313a15103b9b1cc5161e0009ebcb93874c
Alan Slater <alan.slater@nearform.com>
2018-03-13 23:12:33 +0000

    Tests on fake data with many types of overlap

---
+++ test/visualizer-callback-event.test.js
@@ -0,0 +1,137 @@
+'use strict'
+
+const test = require('tap').test
+const CallbackEvent = require('../visualizer/data/callback-event.js')
+const {
+  clusterNodes,
+  aggregateNodes,
+  dummyCallbackEvents,
+  expectedClusterResults,
+  expectedAggregateResults
+} = require('./visualizer-util/fake-overlapping-nodes.js')
+
+// Prepare fake data:
+
+function emptyStats (stop) {
+  return {
+    async: {
+      between: 0,
+      within: 0
+    },
+    sync: 0,
+    rawTotals: stop ? null : emptyStats(true)
+  }
+}
+
+class TestClusterNode {
+  constructor (clusterId) {
+    Object.assign(this, clusterNodes.get(clusterId))
+    this.id = this.clusterId = clusterId
+    this.nodes = new Map()
+    this.stats = emptyStats()
+    return this
+  }
+}
+
+class TestAggregateNode {
+  constructor (aggregateId) {
+    Object.assign(this, aggregateNodes.get(aggregateId))
+    this.id = this.aggregateId = aggregateId
+    this.stats = emptyStats()
+    return this
+  }
+}
+
+for (const [aggregateId] of aggregateNodes) {
+  aggregateNodes.set(aggregateId, new TestAggregateNode(aggregateId))
+}
+
+for (const [clusterId] of clusterNodes) {
+  clusterNodes.set(clusterId, new TestClusterNode(clusterId))
+}
+
+for (let dummyEvent of dummyCallbackEvents) {
+  dummyEvent.init = dummyEvent.delayStart
+  dummyEvent.before = [dummyEvent.before]
+  dummyEvent.after = [dummyEvent.after]
+
+  dummyEvent.aggregateNode = aggregateNodes.get(dummyEvent.aggregateId)
+
+  dummyEvent.aggregateNode.clusterNode = clusterNodes.get(dummyEvent.clusterId)
+  dummyEvent.aggregateNode.clusterNode.nodes.set(dummyEvent.aggregateId, dummyEvent.aggregateNode)
+
+  new CallbackEvent(dummyEvent, 0) // eslint-disable-line no-new
+}
+
+CallbackEvent.processAllCallbackEvents()
+
+// Fake data prepared.
+// Run tests:
+
+function validateNumber (num) {
+  return (typeof num === 'number' && !Number.isNaN(num)) ? num : `not a number (${num}, typeof ${typeof num})`
+}
+
+function applyArrayOfObjectKeys (obj, arr) {
+  // For example, passing ({ a: { a1: 'foo', a2: 'bar'}, b: null }, ['a', 'a2']) returns 'bar'
+  const key = arr.shift()
+  if (arr.length) {
+    return applyArrayOfObjectKeys(obj[key], arr)
+  }
+  return obj[key]
+}
+
+function compare (dataNode, resultKeysArray, expected, expectedKeysArray) {
+  if (!expectedKeysArray) {
+    expectedKeysArray = [...resultKeysArray]
+    expectedKeysArray.shift()
+  }
+  const resultKeysString = resultKeysArray.join('.')
+
+  const actualValue = applyArrayOfObjectKeys(dataNode, resultKeysArray)
+  const expectedValue = applyArrayOfObjectKeys(expected, expectedKeysArray)
+
+  if (validateNumber(actualValue) === expectedValue) return ''
+
+  return `Error: ${dataNode.constructor.name} ${dataNode.id} ${resultKeysString} is ${actualValue}, expected ${expectedValue}. \n`
+}
+
+test('Visualizer data - ClusterNode stats from CallbackEvents', function (t) {
+  let errorMessage = ''
+
+  for (const [clusterId, clusterNode] of clusterNodes) {
+    const expected = expectedClusterResults.get(clusterId)
+
+    errorMessage += compare(clusterNode, ['stats', 'async', 'within'], expected)
+    errorMessage += compare(clusterNode, ['stats', 'async', 'between'], expected)
+    errorMessage += compare(clusterNode, ['stats', 'sync'], expected)
+
+    errorMessage += compare(clusterNode, ['stats', 'rawTotals', 'async', 'within'], expected)
+    errorMessage += compare(clusterNode, ['stats', 'rawTotals', 'async', 'between'], expected)
+    errorMessage += compare(clusterNode, ['stats', 'rawTotals', 'sync'], expected)
+  }
+
+  errorMessage = errorMessage || 'Pass'
+  t.equals(errorMessage, 'Pass')
+  t.end()
+})
+
+test('Visualizer data - AggregateNode stats from CallbackEvents', function (t) {
+  let errorMessage = ''
+  for (const [aggregateId, aggregateNode] of aggregateNodes) {
+    const expected = expectedAggregateResults.get(aggregateId)
+
+    errorMessage += compare(aggregateNode, ['stats', 'async', 'between'], expected, ['async'])
+    errorMessage += compare(aggregateNode, ['stats', 'async', 'within'], { alwaysZero: 0 }, ['alwaysZero'])
+    errorMessage += compare(aggregateNode, ['stats', 'sync'], expected)
+
+    errorMessage += compare(aggregateNode, ['stats', 'rawTotals', 'async', 'between'], expected, ['raw'])
+    errorMessage += compare(aggregateNode, ['stats', 'rawTotals', 'async', 'within'], { alwaysZero: 0 }, ['alwaysZero'])
+
+    if (typeof expected.rawSync === 'undefined') expected.rawSync = expected.sync
+    errorMessage += compare(aggregateNode, ['stats', 'rawTotals', 'sync'], expected, ['rawSync'])
+  }
+  errorMessage = errorMessage || 'Pass'
+  t.equals(errorMessage, 'Pass')
+  t.end()
+})
---
+++ test/visualizer-util/fake-overlapping-nodes.js
@@ -0,0 +1,241 @@
+'use strict'
+
+const dummyCallbackEvents = [
+  { i: 'root', delayStart: undefined, before: undefined, after: undefined, aggregateId: 'root', clusterId: 'A' },
+  { i: 0, delayStart: 9, before: 14, after: 15.5, aggregateId: 'a', clusterId: 'A' },
+  { i: 1, delayStart: 13, before: 15, after: 15.5, aggregateId: 'b', clusterId: 'A' },
+  { i: 2, delayStart: 16.5, before: 17, after: 17.5, aggregateId: 'e', clusterId: 'C' },
+  { i: 3, delayStart: 3, before: 17.5, after: 19, aggregateId: 'a', clusterId: 'A' },
+  { i: 4, delayStart: 18.5, before: 19, after: 19.5, aggregateId: 'c', clusterId: 'A' },
+  { i: 5, delayStart: 18, before: 19.5, after: 22, aggregateId: 'c', clusterId: 'A' },
+  { i: 6, delayStart: 18.5, before: 20, after: 21.5, aggregateId: 'c', clusterId: 'A' },
+  { i: 7, delayStart: 20, before: 21, after: 22, aggregateId: 'a', clusterId: 'A' },
+  { i: 8, delayStart: 16, before: 22, after: 23.5, aggregateId: 'd', clusterId: 'B' },
+  { i: 9, delayStart: 20, before: 24, after: 25.5, aggregateId: 'e', clusterId: 'C' },
+  { i: 10, delayStart: 23.5, before: 24, after: 24.5, aggregateId: 'e', clusterId: 'C' },
+  { i: 11, delayStart: 24, before: 25, after: 25.5, aggregateId: 'd', clusterId: 'B' },
+  { i: 12, delayStart: 24.5, before: 25, after: 25.5, aggregateId: 'e', clusterId: 'C' },
+  { i: 13, delayStart: 21, before: 27, after: 28, aggregateId: 'f', clusterId: 'B' },
+  { i: 14, delayStart: 27.5, before: 28, after: 28.5, aggregateId: 'a', clusterId: 'A' },
+  { i: 15, delayStart: 24.5, before: 28.5, after: 29, aggregateId: 'g', clusterId: 'B' },
+  { i: 16, delayStart: 25, before: 29, after: 29.5, aggregateId: 'h', clusterId: 'C' }
+]
+
+const dummyClusterNodes = {
+  A: {},
+  B: {parentClusterId: 'A'},
+  C: {parentClusterId: 'A'}
+}
+
+const dummyAggregateNodes = {
+  root: {},
+  a: {parentAggregateId: 'root'},
+  b: {parentAggregateId: 'root'},
+  c: {parentAggregateId: 'a'},
+  d: {parentAggregateId: 'c'},
+  e: {parentAggregateId: 'b'},
+  f: {parentAggregateId: 'd'},
+  g: {parentAggregateId: 'f'},
+  h: {parentAggregateId: 'a'}
+}
+
+/**
+ * Diagrams showing how the above events and nodes interrelate:
+ *
+ *                    -----------------
+ *                   |*** CLUSTER A ***|
+ *                   |    [ag root]    |
+ *                   |       /  \      |
+ *                   |  [ag a] [ag b]  |
+ *                   |     / \    \    |
+ *                   |[ag c]  \    \   |
+ *                    ---/-----\----\--
+ *                      /       \    \
+ *           ----------/----     \ ---\-----------
+ *          |** CLUSTER B **|     \** CLUSTER C **|
+ *          |      [ag d]   |     |\   [ag e]     |
+ *          |       /       |     | \             |
+ *          |    [ag f]     |     |  \            |
+ *          |     /         |     |   \           |
+ *          |  [ag g]       |     |    [ag h]     |
+ *           ---------------       ---------------
+ *
+ *        letter = async delay (.delayStart -> .before)
+ *             ▓ = sync processing (.before -> .after)
+ *
+ *  i=   | Time -------->      ⒑                  ⒛
+ *       | 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9  Totals:
+ *  -----|-------------------------------------------------------------
+ * [16]Cb|                                                   hhhhhhhh▓  raw:  4
+ * [15]Bw|                                                  gggggggg▓   raw:  4
+ * [14]Aw|                                                        a▓    raw:  0.5
+ * [13]Bw|                                           ffffffffffff▓▓     raw:  6
+ * [12]Cb|                                                  e▓          raw:  0.5
+ * [11]Bb|                                                 dd▓          raw:  1
+ * [10]Cb|                                                e▓            raw:  0.5
+ *  [9]Cb|                                         eeeeeeee▓▓▓          raw:  4
+ *  [8]Bb|                                 dddddddddddd▓▓▓              raw:  6
+ *  [7]Aw|                                         aa▓▓                 raw:  1
+ *  [6]Aw|                                      ccc▓▓▓                  raw:  2.0
+ *  [5]Aw|                                     ccc▓▓▓▓▓                 raw:  1.5
+ *  [4]Aw|                                      c▓                      raw:  0.5
+ *  [3]Aw|       aaaaaaaaaaaaaaaaaaaaaaaaaaaaa▓▓▓                       raw: 14.5
+ *  [2]Cw|                                  e▓                          raw:  0.5
+ *  [1]Aw|                           bbbb▓                              raw:  2
+ *  [0]Aw|                   aaaaaaaaaa▓▓▓                              raw:  5
+ *  -----|------------------------------------------------------------
+ *       | 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9
+ *
+ *  Correct times based on the above:                                   Expected
+ *  -----|------------------------------------------------------------  results (ms):
+ *       | Flattened async by aggregate node:
+ *      a|       aaaaaaaaaaaaaaaaaaaaaaaaaaaaa     aa             a     16
+ *      b|                           bbbb                                2
+ *      c|                                     cccc                      2
+ *      d|                                 dddddddddddd    dd            7
+ *      e|                                  e      eeeeeeeee             5
+ *      f|                                           ffffffffffff        6
+ *      g|                                                  gggggggg     4
+ *      h|                                                   hhhhhhhh    4
+ *  -----|------------------------------------------------------------
+ *       | Flattened async BETWEEN (b) cluster nodes:
+ *      A|                                                               0
+ *      B|                                 BBBBBBBBBBBB    BB            7
+ *      C|                                  C      CCCCCCCC CCCCCCCCC    9
+ *  -----|------------------------------------------------------------
+ *       | Flattened async WITHIN (w) cluster nodes:
+ *      A|       AAAAAAAAAAAAAAAAAAAAAAAAAAAAA AAAAAA             A     18
+ *      B|                                           BBBBBBBBBBBBBBB     7.5
+ *      C|                                                               0
+ *  -----|------------------------------------------------------------
+ *       | Flattened sync by aggregate node:
+ *      a|                             aaa    aaa    aa            a     4.5
+ *      b|                               b   b                           1
+ *      c|                                       cccccc                  3
+ *      d|                                             ddd   d           2
+ *      e|                                                 eee           1.5
+ *      f|                                                       ff      1
+ *      g|                                                          g    0.5
+ *      h|                                                           h   0.5
+ *  -----|------------------------------------------------------------
+ *       | Flattened sync by cluster nodes:
+ *      A|                             AAA    AAAAAAAAA            A     6.5
+ *      B|                                             BBB   B   BB B    3.5
+ *      C|                                  C              CCC       C   2.5
+ *
+ * Raw async stats:
+ *  A. between: 0, within: 26.5; B. between: 7, within: 10; C. between: 13.5, within: 0
+ *  a: 21, b: 2.5, c: 3.5, d: 7, e: 4.5, f: 6, g: 4, h: 4
+ *
+ * Raw sync stats:
+ * A: 10, B: 3.5, C: 3
+ * a: 4.5, b: 1, c: 4.5, d: 2, e: 3, f: 2, g: 0.5, h: 0.5
+ *
+**/
+
+const expectedClusterResults = new Map(Object.entries({
+  A: {
+    async: {
+      within: 18,
+      between: 0
+    },
+    sync: 6.5,
+    rawTotals: {
+      async: {
+        within: 26.5,
+        between: 0
+      },
+      sync: 9.5
+    }
+  },
+  B: {
+    async: {
+      within: 7.5,
+      between: 7
+    },
+    sync: 3.5,
+    rawTotals: {
+      async: {
+        within: 10,
+        between: 7
+      },
+      sync: 3.5
+    }
+  },
+  C: {
+    async: {
+      within: 0,
+      between: 9
+    },
+    sync: 2.5,
+    rawTotals: {
+      async: {
+        within: 0,
+        between: 9.5
+      },
+      sync: 3.5
+    }
+  }
+}))
+
+// Only define .rawSync if it's different to .sync
+const expectedAggregateResults = new Map(Object.entries({
+  root: {
+    async: 0,
+    sync: 0,
+    raw: 0
+  },
+  a: {
+    async: 16,
+    sync: 4.5,
+    raw: 21
+  },
+  b: {
+    async: 2,
+    sync: 0.5,
+    raw: 2
+  },
+  c: {
+    async: 2,
+    sync: 3,
+    raw: 3.5,
+    rawSync: 4.5
+  },
+  d: {
+    async: 7,
+    sync: 2,
+    raw: 7
+  },
+  e: {
+    async: 5,
+    sync: 2,
+    raw: 5.5,
+    rawSync: 3
+  },
+  f: {
+    async: 6,
+    sync: 1,
+    raw: 6
+  },
+  g: {
+    async: 4,
+    sync: 0.5,
+    raw: 4
+  },
+  h: {
+    async: 4,
+    sync: 0.5,
+    raw: 4
+  },
+}))
+
+const clusterNodes = new Map(Object.entries(dummyClusterNodes))
+const aggregateNodes = new Map(Object.entries(dummyAggregateNodes))
+
+module.exports = {
+  clusterNodes,
+  aggregateNodes,
+  dummyCallbackEvents,
+  expectedClusterResults,
+  expectedAggregateResults
+}
