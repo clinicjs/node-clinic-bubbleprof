bc624a49ee0ca719f71ad70f2bcfd9c1d3948240
Kamil Mech <kamil.mech@nearform.com>
2018-03-24 07:52:44 +0000

    ui-B2: Add Stem logic (#51)
    * Add Stem logic
    
    Stem logic is responsible for decorating each ClusterNode and AggregateNode with meta information about its "stem length" which is a sum of lengths and diameters of all its ancestors and its own
    
    * Remove obsolete todo comment
    
    * Adding node lookup methods (#52)
    
    * Restore stems files
    
    * Simplify stem ancestry
    
    * Apply latest node conveniences
    
    * Use two consts instead of variable recycling
    
    * Justify non-explicit Stem class export
    
    * Replace destructor with built-in Map iterator
    
    * Justify toFixed() in tests
    
    * Refactor `getNodePath` to a more accurate name
    
    * Remove now obsolete `nodeByType`

---
+++ visualizer/data/stems.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const { isNumber } = require('./validation.js')
+
+function getNodeAncestorIds (node) {
+  const parent = node.getParentNode()
+  return parent ? [...parent.stem.ancestors.ids, parent.id] : []
+}
+
+function calculateRadius (circumference) {
+  return circumference / (2 * Math.PI)
+}
+
+class Stem {
+  constructor (node) {
+    this.ancestors = {
+      totalBetween: 0,
+      totalDiameter: 0,
+      ids: getNodeAncestorIds(node)
+    }
+    // this.ownBetween = node.getStat('between') // TODO: uncomment as soon as averaging is implemented
+    this.ownBetween = node.stats.async.between
+    // this.ownDiameter = calculateRadius(node.getStat('within')) * 2 // TODO: uncomment as soon as averaging is implemented
+    this.ownDiameter = calculateRadius(node.stats.async.within) * 2
+
+    this._totalStemLengthByScale = {}
+
+    for (const ancestorId of this.ancestors.ids) {
+      const ancestor = node.getSameType(ancestorId)
+      this.ancestors.totalBetween += ancestor.stem.ownBetween
+      this.ancestors.totalDiameter += ancestor.stem.ownDiameter
+    }
+  }
+  getTotalStemLength (scale = 1) {
+    if (!isNumber(this._totalStemLengthByScale[scale])) {
+      // All lines and circumferences are meant to represent same value / pixel ratio => so simply multiply all by same scale
+      this._totalStemLengthByScale[scale] = (this.ancestors.totalBetween + this.ancestors.totalDiameter + this.ownBetween + this.ownDiameter) * scale
+    }
+    return this._totalStemLengthByScale[scale]
+  }
+}
+
+module.exports = {
+  Stem
+  // ClumpedPyramidLayout will be added and exported in delivery for ui-D2
+}
