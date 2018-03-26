5d3b8170186824171402a0773728a6daa19c63dd
Alan Slater <alan.slater@nearform.com>
2018-03-26 22:10:48 +0100

    More comments, clearer chaining

--- visualizer/layout/index.js
+++ visualizer/layout/index.js
@@ -5,6 +5,9 @@ const { Stem } = require('./stems.js')
 
 function generateLayout (callback, dataSet, settings) {
   const layout = new Layout(dataSet, settings)
+
+  // Two steps here so we can hook in modifications to the properties before proceeding
+  // e.g. for tests etc similar to with data/index.js
   layout.generate()
   callback(null, layout)
 }
--- visualizer/main.js
+++ visualizer/main.js
@@ -2,12 +2,25 @@
 
 const loadData = require('./data/index.js')
 const generateLayout = require('./layout/index.js')
+// then something like based on ui-E1 but made consistent:
+// const drawUi = require('./draw-ui/index.js')
+// ...which creates an instance of BubbleprofUi from ./draw-ui/bubbleprof-ui.js
+// ...which creates from many subclasses
 
 loadData(function maybeDone (err, data) {
   if (err) throw err
   window.data = data
   console.log('data is exposed on window.data')
 
-  generateLayout(data)
-  // TODO: then continue to draw as in ui-E1 branch
+  generateLayout(function (err, layout) {
+    if (err) throw err
+      window.layout = layout
+      console.log('layout is exposed on window.layout')
+
+      // TODO: then continue to draw, based on ui-E1 branch but made consistent, like:
+      //
+      // drawUi(function (err, ui)) {
+      //   some action on complete e.g. hide a loading status bar
+      // }, layout)
+  }, data)
 })
