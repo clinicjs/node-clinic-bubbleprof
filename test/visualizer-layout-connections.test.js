ff59613c4754273edc96278a0a547be86446d642
Kamil Mech <kamil.mech@nearform.com>
2018-03-30 15:39:25 +0100

    Ui-C1.5 - Refactor & combine Connection, Scale, Stem under Layout (#56)
    * Basic outline of Scale class, with settings
    
    * Move stems.js, add index.js to layout dir
    
    * Move validation.js helpers to main dir
    
    * Re-use existing radius calc function
    
    * Move main flow to main.js
    
    * Fix tests
    
    * Remove obsolete require
    
    * Add connections class
    
    * Add missing coverage in DataSet
    
    * Make fixes and add coverage
    
    * Proposed layout class structure
    
    * More comments, clearer chaining
    
    * Give Scale same settings as Layout
    
    * Suggestion for improving clarity of loops
    
    * Update, fix, refactor, clean up
    
    - Fix tests and coverage
    - Refactor async flow into sync flow
    - Rename `data` vars to `dataSet`
    - Update stems tests namespacing to point at `layout` instead of `data`
    
    * Add coverage for the layout class
    
    * Tweak comments
    
    * Remove obsolete wip comments
    
    * Export Stem directly and remove placeholder comments

--- test/visualizer-layout-connections.test.js
+++ test/visualizer-layout-connections.test.js
@@ -16,62 +16,56 @@ const fakeScale = {
 }
 
 test('Visualizer layout - scale - calculates visible circle radius based on within of the node and the scale', function (t) {
-  loadData((err, data) => {
-    t.ifError(err)
+  const dataSet = loadData(slowioJson)
 
-    const parentNode = data.getByNodeType('ClusterNode', 1)
-    const childNode = data.getByNodeType('ClusterNode', 3)
+  const parentNode = dataSet.getByNodeType('ClusterNode', 1)
+  const childNode = dataSet.getByNodeType('ClusterNode', 3)
 
-    const connection = new Connection(parentNode, childNode, fakeScale)
+  const connection = new Connection(parentNode, childNode, fakeScale)
 
-    t.ok(isNumber(parentNode.getWithinTime()))
-    const expectedParentRadius = fakeScale.getCircleRadius(parentNode.getWithinTime())
-    t.equal(connection.getSourceRadius(), expectedParentRadius)
+  t.ok(isNumber(parentNode.getWithinTime()))
+  const expectedParentRadius = fakeScale.getCircleRadius(parentNode.getWithinTime())
+  t.equal(connection.getSourceRadius(), expectedParentRadius)
 
-    t.ok(isNumber(childNode.getWithinTime()))
-    const expectedChildRadius = fakeScale.getCircleRadius(childNode.getWithinTime())
-    t.equal(connection.getTargetRadius(), expectedChildRadius)
+  t.ok(isNumber(childNode.getWithinTime()))
+  const expectedChildRadius = fakeScale.getCircleRadius(childNode.getWithinTime())
+  t.equal(connection.getTargetRadius(), expectedChildRadius)
 
-    t.end()
-  }, slowioJson)
+  t.end()
 })
 
 test('Visualizer layout - scale - calculates visible line length based on between of the child node and the scale', function (t) {
-  loadData((err, data) => {
-    t.ifError(err)
+  const dataSet = loadData(slowioJson)
 
-    const parentNode = data.getByNodeType('ClusterNode', 1)
-    const childNode = data.getByNodeType('ClusterNode', 3)
+  const parentNode = dataSet.getByNodeType('ClusterNode', 1)
+  const childNode = dataSet.getByNodeType('ClusterNode', 3)
 
-    const connection = new Connection(parentNode, childNode, fakeScale)
-    t.ok(isNumber(childNode.getBetweenTime()))
-    const expectedVisibleLength = fakeScale.getLineLength(childNode.getBetweenTime())
-    t.equal(connection.getVisibleLineLength(), expectedVisibleLength)
+  const connection = new Connection(parentNode, childNode, fakeScale)
+  t.ok(isNumber(childNode.getBetweenTime()))
+  const expectedVisibleLength = fakeScale.getLineLength(childNode.getBetweenTime())
+  t.equal(connection.getVisibleLineLength(), expectedVisibleLength)
 
-    t.end()
-  }, slowioJson)
+  t.end()
 })
 
 test('Visualizer layout - scale - calculates distance between centers', function (t) {
-  loadData((err, data) => {
-    t.ifError(err)
+  const dataSet = loadData(slowioJson)
 
-    const parentNode = data.getByNodeType('ClusterNode', 1)
-    const childNode = data.getByNodeType('ClusterNode', 3)
+  const parentNode = dataSet.getByNodeType('ClusterNode', 1)
+  const childNode = dataSet.getByNodeType('ClusterNode', 3)
 
-    const connection = new Connection(parentNode, childNode, fakeScale)
+  const connection = new Connection(parentNode, childNode, fakeScale)
 
-    const expectedParentRadius = fakeScale.getCircleRadius(parentNode.getWithinTime())
-    const expectedChildRadius = fakeScale.getCircleRadius(childNode.getWithinTime())
-    const expectedVisibleLength = fakeScale.getLineLength(childNode.getBetweenTime())
+  const expectedParentRadius = fakeScale.getCircleRadius(parentNode.getWithinTime())
+  const expectedChildRadius = fakeScale.getCircleRadius(childNode.getWithinTime())
+  const expectedVisibleLength = fakeScale.getLineLength(childNode.getBetweenTime())
 
-    const expectedDistance = expectedParentRadius +
-                              expectedChildRadius +
-                              expectedVisibleLength +
-                              (fakeScale.settings.labelMinimumSpace * 2) +
-                              fakeScale.settings.lineWidth
-    t.equal(connection.getDistanceBetweenCenters(), expectedDistance)
+  const expectedDistance = expectedParentRadius +
+                            expectedChildRadius +
+                            expectedVisibleLength +
+                            (fakeScale.settings.labelMinimumSpace * 2) +
+                            fakeScale.settings.lineWidth
+  t.equal(connection.getDistanceBetweenCenters(), expectedDistance)
 
-    t.end()
-  }, slowioJson)
+  t.end()
 })
---
+++ test/visualizer-layout-stems.test.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const test = require('tap').test
+const loadData = require('../visualizer/data/index.js')
+const { isNumber } = require('../visualizer/validation.js')
+const slowioJson = require('./visualizer-util/sampledata-slowio.json')
+const generateLayout = require('../visualizer/layout/index.js')
+
+test('Visualizer layout - stems - calculates between and diameter based on stats', function (t) {
+  const dataSet = loadData(slowioJson)
+  generateLayout(dataSet)
+
+  const node = dataSet.clusterNodes.get(16)
+  const stem = node.stem
+  t.equal(stem.ownBetween, node.getBetweenTime())
+  t.equal(stem.ownDiameter, node.getWithinTime() / Math.PI)
+
+  t.end()
+})
+
+test('Visualizer layout - stems - calculates length based on ancestors', function (t) {
+  const dataSet = loadData(slowioJson)
+  generateLayout(dataSet)
+
+  const stem = dataSet.clusterNodes.get(16).stem
+  const totalStemLength = stem.getTotalStemLength()
+  t.deepEqual(stem.ancestors.ids, [ 1, 5, 7, 8, 10 ])
+  t.equal(totalStemLength.toFixed(8), '21897.14445863')
+
+  const toOwnLength = id => {
+    const ancestorStem = dataSet.clusterNodes.get(id).stem
+    return ancestorStem.ownBetween + ancestorStem.ownDiameter
+  }
+  const sum = (a, b) => a + b
+  const totalAncestorsLength = stem.ancestors.ids.map(toOwnLength).reduce(sum, 0)
+  // Floating point precision acting up here, hence `.toFixed()` both sides
+  t.equal((totalStemLength - totalAncestorsLength).toFixed(8), (stem.ownBetween + stem.ownDiameter).toFixed(8))
+
+  t.end()
+})
+
+test('Visualizer layout - stems - caches length by scale', function (t) {
+  const dataSet = loadData(slowioJson)
+  generateLayout(dataSet)
+
+  const stem = dataSet.clusterNodes.get(16).stem
+  t.equal(stem.getTotalStemLength(), stem._totalStemLengthByScale[1])
+  t.ok(isNumber(stem.getTotalStemLength()))
+  t.equal(stem.getTotalStemLength(5), stem._totalStemLengthByScale[5])
+  t.ok(isNumber(stem.getTotalStemLength(5)))
+
+  t.end()
+})
