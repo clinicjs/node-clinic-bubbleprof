4e1750f7d643a834aba6c8d1524d05c84955ec32
Kamil Mech <kamil.mech@nearform.com>
2018-03-30 12:02:26 +0100

    Remove obsolete wip comments

--- visualizer/main.js
+++ visualizer/main.js
@@ -2,10 +2,6 @@
 
 const loadData = require('./data/index.js')
 const generateLayout = require('./layout/index.js')
-// then something like based on ui-E1 but made consistent:
-// const drawUi = require('./draw-ui/index.js')
-// ...which creates an instance of BubbleprofUi from ./draw-ui/bubbleprof-ui.js
-// ...which creates from many subclasses
 
 const dataSet = loadData()
 window.data = dataSet
@@ -14,8 +10,3 @@ console.log('data is exposed on window.data')
 const layout = generateLayout(dataSet)
 window.layout = layout
 console.log('layout is exposed on window.layout')
-
-// TODO: then continue to draw, based on ui-E1 branch but made consistent, like:
-//
-// const ui = drawUi()
-// some action on complete e.g. hide a loading status bar
