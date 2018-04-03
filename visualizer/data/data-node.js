7da56a9c83bf847f64d22fdd5ab8d4594d340dfb
Alan Slater <alan.slater@nearform.com>
2018-04-03 07:20:20 +0100

    Move Frame class to its own file

--- visualizer/data/data-node.js
+++ visualizer/data/data-node.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const Frame = require('./frame.js')
 const { CallbackEvent } = require('./callback-event.js')
 const { isNumber } = require('../validation.js')
 
@@ -122,49 +123,6 @@ class AggregateNode extends DataNode {
   }
   get parentId () {
     return this.parentAggregateId
-  }
-}
-
-class Frame {
-  constructor (frame) {
-    this.functionName = frame.functionName
-    this.typeName = frame.typeName
-    this.evalOrigin = frame.evalOrigin
-    this.fileName = frame.fileName
-    this.lineNumber = frame.lineNumber
-    this.columnNumber = frame.columnNumber
-    this.isEval = frame.isEval
-    this.isConstructor = frame.isConstructor
-    this.isNative = frame.isNative
-    this.isToplevel = frame.isToplevel
-  }
-
-  format () {
-    // Get name
-    let name = this.functionName ? this.functionName : '<anonymous>'
-    if (this.isEval) {
-      // no change
-    } else if (this.isToplevel) {
-      // no change
-    } else if (this.isConstructor) {
-      name = 'new ' + name
-    } else if (this.isNative) {
-      name = 'native ' + name
-    } else {
-      name = this.typeName + '.' + name
-    }
-
-    // Get position
-    let formatted = '    at ' + name
-    if (this.isEval) {
-      formatted += ' ' + this.evalOrigin
-    } else {
-      formatted += ' ' + this.fileName
-      formatted += ':' + (this.lineNumber > 0 ? this.lineNumber : '')
-      formatted += (this.columnNumber > 0 ? ':' + this.columnNumber : '')
-    }
-
-    return formatted
   }
 }
 
