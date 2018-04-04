f902903bb75f71961985070648ebd3844c67c38a
Kamil Mech <kamil.mech@nearform.com>
2018-04-04 11:42:14 +0100

    Move topology mock to its own file

--- test/visualizer-layout-positioning.test.js
+++ test/visualizer-layout-positioning.test.js
@@ -6,72 +6,14 @@ const generateLayout = require('../visualizer/layout/index.js')
 const Positioning = require('../visualizer/layout/positioning.js')
 const shuffle = require('shuffle-array')
 
-function mockClusterNode ({ id, parentId, children, stemLength }) {
-  const clusterNode = {
-    // ClusterNode
-    clusterId: id,
-    parentClusterId: parentId,
-    nodes: [
-      {
-        // AggregateNode
-        aggregateId: id,
-        parentAggregateId: parentId,
-        children: [],
-        frames: [],
-        sources: [
-          {
-            // SourceNode
-            asyncId: id,
-            i: id,
-            init: 0,
-            before: [stemLength],
-            after: [stemLength],
-            aggregateId: id,
-            clusterId: id
-          }
-        ]
-      }
-    ],
-    children
-  }
-  if (parentId === 0) {
-    clusterNode.isRoot = true
-  }
-  return clusterNode
-}
-
-function mockTopology (topology) {
-  const clusterNodes = new Map()
-  for (const instruction of topology) {
-    const ids = instruction[0].split('.').map(id => parseInt(id))
-    const totalStemLength = instruction[1]
-    const lastId = ids[ids.length - 1]
-    for (let i = 0; i < ids.length; ++i) {
-      const id = ids[i]
-      if (clusterNodes.get(id)) {
-        continue
-      }
-      const parentId = ids[i - 1] || 0
-      const fillerValue = parentId === 0 ? Math.PI : 1 // Root's value is within, not between
-      const clusterNode = mockClusterNode({
-        id,
-        parentId,
-        children: ids.slice(i + 1, ids.length),
-        stemLength: id === lastId ? totalStemLength : fillerValue
-      })
-      clusterNodes.set(id, clusterNode)
-    }
-  }
-  return mapToArray(clusterNodes)
-}
-
-function topologyToOrderedLeaves (topology) {
-  return topology.map(d => d[0].split('.').map(i => parseInt(i)).reverse()[0])
-}
-
 function mapToArray (map) {
   return [...map.values()]
 }
+
+const {
+  mockTopology,
+  topologyToOrderedLeaves
+} = require('./visualizer-util/fake-topology.js')
 
 test('Visualizer layout - positioning - mock topology', function (t) {
   const topology = [
