6c7285626b6bbf6a40ded5196cf08edbbc0f2392
Kamil Mech <kamil.mech@nearform.com>
2018-04-12 16:03:41 +0100

    Support node collapsing

--- test/visualizer-layout.test.js
+++ test/visualizer-layout.test.js
@@ -2,29 +2,116 @@
 
 const test = require('tap').test
 const loadData = require('../visualizer/data/index.js')
-const slowioJson = require('./visualizer-util/sampledata-slowio.json')
 const generateLayout = require('../visualizer/layout/index.js')
+const Layout = require('../visualizer/layout/layout.js')
 
-/* TODO: see below
-function mapToArray (map) {
-  return [...map.values()]
-}
-*/
+const {
+  mockTopology
+} = require('./visualizer-util/fake-topology.js')
 
-test('Visualizer layout - stems - calculates between and diameter based on stats', function (t) {
-  const dataSet = loadData(slowioJson)
-  const layout = generateLayout(dataSet)
+// T=Tiny, L=Long, C=Collapsed
 
-  t.equal(layout.connections.length, dataSet.clusterNodes.size - 1)
-  /* TODO: rework this to loop through aggregateNodes in the PR that contains step down
-  t.equal(layout.aggregateConnections.size, dataSet.clusterNodes.size)
-  for (const [clusterId, aggregateConnectionsByClusterId] of layout.aggregateConnections) {
-    const aggregateNodes = dataSet.getByNodeType('ClusterNode', clusterId).nodes
-    const includesRootAggregateNode = mapToArray(aggregateNodes).find(aggregateNode => aggregateNode.isRoot)
-    const expectedSize = includesRootAggregateNode ? aggregateNodes.size - 1 : aggregateNodes.size
-    t.equal(aggregateConnectionsByClusterId.length, expectedSize)
-  }
-  */
+// T->T->T->L gives C->L
+test('Visualizer layout - collapse - collapses children and parents linearly', function (t) {
+  const topology = [
+    ['1.2.3.4', 100]
+  ]
+  const dataSet = loadData(mockTopology(topology))
+  t.ok(dataSet)
+  const layout = generateLayout(dataSet, { labelMinimumSpace: 0, lineWidth: 0 })
+  t.ok(layout)
+  const collapsed = Layout.collapseNodes(layout.nodes, { scaleFactor: 1 })
+  const actual = collapsed.map(ch => ch.id || ch.children.map(ch => ch.id))
+  t.deepEqual(actual, [[1, 2, 3], 4])
+
+  t.end()
+})
+
+// L->T->L
+//  \>T->L
+// gives
+// L->C->L
+//     \>L
+test('Visualizer layout - collapse - collapses branches at stem', function (t) {
+  const topology = [
+    ['1.2.3', 100],
+    ['1.4.5', 100]
+  ]
+  const dataSet = loadData(mockTopology(topology))
+  t.ok(dataSet)
+  dataSet.clusterNodes.get(1).stats.async.between = 100 // make root long
+  const layout = generateLayout(dataSet, { labelMinimumSpace: 0, lineWidth: 0 })
+  t.ok(layout)
+  const collapsed = Layout.collapseNodes(layout.nodes, { scaleFactor: 1 })
+  const actual = collapsed.map(ch => ch.id || ch.children.map(ch => ch.id))
+  t.deepEqual(actual, [1, [2, 4], 3, 5])
+
+  t.end()
+})
+
+// T->T->L
+//  \>T->L
+// gives
+// C->L
+//  \>L
+test('Visualizer layout - collapse - collapses both siblings and parents', function (t) {
+  const topology = [
+    ['1.2.3', 100],
+    ['1.4.5', 100]
+  ]
+  const dataSet = loadData(mockTopology(topology))
+  t.ok(dataSet)
+  const layout = generateLayout(dataSet, { labelMinimumSpace: 0, lineWidth: 0 })
+  t.ok(layout)
+  const collapsed = Layout.collapseNodes(layout.nodes, { scaleFactor: 1 })
+  const actual = collapsed.map(ch => ch.id || ch.children.map(ch => ch.id))
+  t.deepEqual(actual, [[1, 2, 4], 3, 5])
+
+  t.end()
+})
+
+// T->T->L
+//  \>L->L
+// gives
+// C->L
+//  \>L
+test('Visualizer layout - collapse - collapses both siblings and parents', function (t) {
+  const topology = [
+    ['1.2.3', 100],
+    ['1.4.5', 100]
+  ]
+  const dataSet = loadData(mockTopology(topology))
+  t.ok(dataSet)
+  dataSet.clusterNodes.get(4).stats.async.between = 100 // make node 4 long
+  const layout = generateLayout(dataSet, { labelMinimumSpace: 0, lineWidth: 0 })
+  t.ok(layout)
+  const collapsed = Layout.collapseNodes(layout.nodes, { scaleFactor: 1 })
+  const actual = collapsed.map(ch => ch.id || ch.children.map(ch => ch.id))
+  t.deepEqual(actual, [[1, 2], 3, 4, 5])
+
+  t.end()
+})
+
+// T->T->L
+//  \>L->L
+// gives
+// C->L
+//  \>L
+test('Visualizer layout - collapse - works with missing leaves', function (t) {
+  const topology = [
+    ['1.2.3', 100],
+    ['1.4.5', 100]
+  ]
+  const dataSet = loadData(mockTopology(topology))
+  t.ok(dataSet)
+  dataSet.clusterNodes.get(4).stats.async.between = 100 // make node 4 long
+  const subset = [1, 2, 3, 4].map(nodeId => dataSet.clusterNodes.get(nodeId))
+  const layout = new Layout(subset, { labelMinimumSpace: 0, lineWidth: 0 })
+  t.ok(layout)
+  layout.prepareLayoutNodes()
+  const collapsed = Layout.collapseNodes(layout.nodes, { scaleFactor: 1 })
+  const actual = collapsed.map(ch => ch.id || ch.children.map(ch => ch.id))
+  t.deepEqual(actual, [[1, 2], 3, 4])
 
   t.end()
 })
