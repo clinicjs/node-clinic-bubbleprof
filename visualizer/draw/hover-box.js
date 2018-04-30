783423b4806204d3a2a38ce22a6254a14b5e053f
Alan Slater <alan.slater@nearform.com>
2018-04-30 11:45:47 +0100

    Add clarifying comments, move .on to end

--- visualizer/draw/hover-box.js
+++ visualizer/draw/hover-box.js
@@ -16,23 +16,25 @@ class HoverBox extends HtmlContent {
     this.d3Element.classed('hover-box', true)
     this.d3Element.classed('hidden', true)
 
-    this.ui.on('hover', layoutNode => {
-      this.isHidden = !layoutNode
-      this.draw(layoutNode)
-    })
-
     this.d3Title = this.d3Element.append('h2')
     this.d3Subtitle = this.d3Title.append('span')
       .classed('subtitle', true)
 
     this.betweenTime = this.d3Element.append('p')
     this.withinTime = this.d3Element.append('p')
+
+    this.ui.on('hover', layoutNode => {
+      this.isHidden = !layoutNode
+      this.draw(layoutNode)
+    })
   }
 
   draw (layoutNode) {
     super.draw()
 
     if (layoutNode) {
+      // Mouseover on the hover box itself causes mouseout of the element that showed the hover box
+      // Re-show hover box so mouse/trackpad users can interact with hover content e.g. select text
       this.d3Element.on('mouseover', () => {
         this.ui.highlightNode(layoutNode)
       })
@@ -41,6 +43,8 @@ class HoverBox extends HtmlContent {
         this.ui.highlightNode(null)
       })
 
+      // The SVG element scales like a responsive image, the HTML hover box doesn't
+      // So the exact pixel position of the hover box depends on the SVG's current width
       const svg = this.contentProperties.svg
       const svgWidth = svg.d3Element.node().getBoundingClientRect().width
       const responsiveScaleFactor = svgWidth / svg.svgBounds.width
