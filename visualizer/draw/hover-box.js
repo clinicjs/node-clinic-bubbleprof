09f2dea1d7abfadc28e8dde967dadf909ecd0e44
Alan Slater <alan.slater@nearform.com>
2018-05-01 17:41:18 +0300

    Show in hover box what clicking a node will do (#110)
    * Move hover emits to ui.highlightNode()
    
    It's better to have event emitters all in one place
    
    * Add clarifying comments, move .on to end
    
    * Show hover box in sublayouts
    
    * Remove console.log added in 0.7.0
    
    * Seperate ShortcutNodes from ArtificalNodes
    
    This makes it easier to distinguish if an artificial data node is a link/shortcut to another node, and stops adding a meaningless 'linkTo' property to the collapsed dataNodes. Also changing the naming from 'link' to 'shorcut' because we use 'link' to mean the line drawn between nodes.
    
    * Make hover box tell user what clicking does
    
    ### Redesign
    
    * Click message at top, not styled like button
    
    * Make click message clickable
    
    * Flip lower hover box if goes off bottom
    
    * Move click event to entire title block
    
    * Stop click propagation closing sublayout
    
    * Show time content for shortcutNodes
    
    * Make click message bold

--- visualizer/draw/hover-box.js
+++ visualizer/draw/hover-box.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// const d3 = require('./d3-subset.js') // Currently unused but will be used
+const d3 = require('./d3-subset.js')
 const HtmlContent = require('./html-content.js')
 
 class HoverBox extends HtmlContent {
@@ -16,33 +16,48 @@ class HoverBox extends HtmlContent {
     this.d3Element.classed('hover-box', true)
     this.d3Element.classed('hidden', true)
 
+    this.d3TitleBlock = this.d3Element.append('div')
+      .classed('block', true)
+      .classed('title-block', true)
+
+    this.d3Title = this.d3TitleBlock.append('h2')
+    this.d3ClickMessage = this.d3TitleBlock.append('a')
+      .classed('click-message', true)
+
+    this.d3TimeBlock = this.d3Element.append('div')
+      .classed('block', true)
+      .classed('time-block', true)
+
+    this.d3BetweenTime = this.d3TimeBlock.append('p')
+    this.d3WithinTime = this.d3TimeBlock.append('p')
+
     this.ui.on('hover', layoutNode => {
       this.isHidden = !layoutNode
       this.draw(layoutNode)
     })
-
-    this.d3Title = this.d3Element.append('h2')
-    this.d3Subtitle = this.d3Title.append('span')
-      .classed('subtitle', true)
-
-    this.betweenTime = this.d3Element.append('p')
-    this.withinTime = this.d3Element.append('p')
   }
 
   draw (layoutNode) {
     super.draw()
 
     if (layoutNode) {
+      // Mouseover on the hover box itself causes mouseout of the element that showed the hover box
+      // Re-show hover box so mouse/trackpad users can interact with hover content e.g. select text
       this.d3Element.on('mouseover', () => {
-        this.ui.emit('hover', layoutNode)
+        this.ui.highlightNode(layoutNode)
       })
 
       this.d3Element.on('mouseout', () => {
-        this.ui.emit('hover', null)
+        this.ui.highlightNode(null)
       })
 
+      // The SVG element scales like a responsive image, the HTML hover box doesn't
+      // So the exact pixel position of the hover box depends on the SVG's current width
       const svg = this.contentProperties.svg
-      const svgWidth = svg.d3Element.node().getBoundingClientRect().width
+      const {
+        width: svgWidth,
+        height: svgHeight
+      } = svg.d3Element.node().getBoundingClientRect()
       const responsiveScaleFactor = svgWidth / svg.svgBounds.width
 
       const nodePosition = layoutNode.position
@@ -52,11 +67,38 @@ class HoverBox extends HtmlContent {
       this.d3Element.style('top', top + 'px')
       this.d3Element.style('left', left + 'px')
 
-      const node = layoutNode.node
-      this.d3Title.text(node.name)
-      this.d3Subtitle.text(`${node.constructor.name} #${node.id})`)
-      this.betweenTime.html(`<strong>${this.ui.formatNumber(node.getBetweenTime())}\u2009ms</strong> aggregated delay from the previous bubble.`)
-      this.withinTime.html(`<strong>${this.ui.formatNumber(node.getWithinTime())}\u2009ms</strong> aggregated delay within this bubble.`)
+      const nodeType = layoutNode.node.constructor.name
+      const dataNode = nodeType === 'ShortcutNode' ? layoutNode.node.shortcutTo : layoutNode.node
+
+      this.d3Title.text(dataNode.name)
+
+      this.d3BetweenTime.html(`<strong>${this.ui.formatNumber(dataNode.getBetweenTime())}\u2009ms</strong> aggregated delay from the previous bubble.`)
+      this.d3WithinTime.html(`<strong>${this.ui.formatNumber(dataNode.getWithinTime())}\u2009ms</strong> aggregated delay within this bubble.`)
+
+      switch (nodeType) {
+        case 'AggregateNode':
+          this.d3ClickMessage.text('Click to display stack trace')
+          this.d3Element.attr('name', 'aggregate-node')
+          break
+        case 'ShortcutNode':
+          this.d3ClickMessage.text(`Click to navigate to "${dataNode.name}"`)
+          this.d3Element.attr('name', 'shortcut-node')
+          break
+        case 'ArtificialNode':
+        case 'ClusterNode':
+          const nodesCount = nodeType === 'ClusterNode' ? dataNode.nodes.size : layoutNode.collapsedNodes.length
+          this.d3ClickMessage.text(`Click to expand ${nodesCount} grouped async_hooks`)
+          this.d3Element.attr('name', 'cluster-node')
+          break
+      }
+      this.d3TitleBlock.on('click', () => {
+        d3.event.stopPropagation()
+        this.ui.highlightNode(null)
+        this.ui.selectNode(layoutNode)
+      })
+
+      const boxHeight = this.d3Element.node().getBoundingClientRect().height
+      this.d3Element.classed('off-bottom', top + boxHeight > svgHeight)
     }
   }
 }
