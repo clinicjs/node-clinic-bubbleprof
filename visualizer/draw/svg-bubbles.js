f06906c7b6d88ae38c50285d60b544481a9cf18a
Alan Slater <alan.slater@nearform.com>
2018-05-01 16:14:37 +0100

    Revert "Have bubble and link time labels match"
    This reverts commit 6e3ab41f4f0eb79c0c625355180a0de76d0ec2a5.

--- visualizer/draw/svg-bubbles.js
+++ visualizer/draw/svg-bubbles.js
@@ -59,9 +59,9 @@ class Bubbles extends SvgContentGroup {
       })
 
     this.addCircles()
-    if (this.nodeType === 'ClusterNode') this.addTypeDonuts()
-
     this.addLabels()
+
+    if (this.nodeType === 'ClusterNode') this.addTypeDonuts()
   }
 
   getNodePosition (layoutNode) {
