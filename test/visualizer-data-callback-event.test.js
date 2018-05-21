507289510e3de4a357759c7ae2f943f2c89b9cd6
Alan Slater <alan.slater@nearform.com>
2018-05-21 14:34:15 +0100

    Tests for time segment getting and setting

--- test/visualizer-data-callback-event.test.js
+++ test/visualizer-data-callback-event.test.js
@@ -102,3 +102,151 @@ test('Visualizer CallbackEvents - Invalid data item', function (t) {
 
   t.end()
 })
+
+test('Visualizer CallbackEvents - Wall time slices', function (t) {
+  const {
+    profileStart,
+    profileEnd,
+    profileDuration,
+    msPerPercent,
+    getSegments
+  } = dataSet.wallTime
+
+  t.equals(profileStart, 3)
+  t.equals(profileEnd, 29.5)
+  t.equals(profileDuration, 26.5)
+  t.equals(msPerPercent, 0.265)
+
+  // Simple slice containing two instances of one aggregate node
+  const sliceA = getSegments(10, 11)
+  t.equals(sliceA.length, 5)
+
+  const expected = {
+    asyncAggregateIds: new Set(['a']),
+    asyncAsyncIds: new Set([1, 4]),
+    syncIds: new Set()
+  }
+
+  let i
+  for (i = 0; i < sliceA.length; i++) {
+    const {
+      asyncPending,
+      syncActive
+    } = sliceA[i]
+
+    t.equals(syncActive.callbackCount, 0)
+    t.strictSame(syncActive.aggregateNodes, expected.syncIds)
+    t.strictSame(syncActive.asyncIds, expected.syncIds)
+
+    t.equals(asyncPending.callbackCount, 2)
+    t.strictSame(asyncPending.aggregateNodes, expected.asyncAggregateIds)
+    t.strictSame(asyncPending.asyncIds, expected.asyncAsyncIds)
+  }
+
+  // More complex slice
+  const sliceB = getSegments(22.5, 25)
+  t.equals(sliceB.length, 11)
+
+  for (i = 0; i < sliceB.length; i++) {
+    const {
+      asyncPending,
+      syncActive
+    } = sliceB[i]
+
+    switch (i) { // Comments show time spans in each 1% segment
+      case 0: // 22.26 - 22.525
+      case 1: // 22.525 - 22.79
+      case 2: // 22.79 - 23.055
+      case 3: // 23.055 - 23.32
+        getSegments(22.26, 23.32, true).forEach((segment) => t.strictSame(segment, sliceB[i]))
+
+        t.equals(syncActive.callbackCount, 1)
+        t.strictSame(syncActive.aggregateNodes, new Set(['d']))
+        t.strictSame(syncActive.asyncIds, new Set([9]))
+
+        t.equals(asyncPending.callbackCount, 2)
+        t.strictSame(asyncPending.aggregateNodes, new Set(['e', 'f']))
+        t.strictSame(asyncPending.asyncIds, new Set([10, 12]))
+        break
+      case 4: // 23.32 - 23.585
+        getSegments(23.4, 23.585, true).forEach((segment) => t.strictSame(segment, sliceB[i]))
+
+        t.equals(syncActive.callbackCount, 1)
+        t.strictSame(syncActive.aggregateNodes, new Set(['d']))
+        t.strictSame(syncActive.asyncIds, new Set([9]))
+
+        t.equals(asyncPending.callbackCount, 4)
+        t.strictSame(asyncPending.aggregateNodes, new Set(['d', 'e', 'f']))
+        t.strictSame(asyncPending.asyncIds, new Set([9, 10, 11, 12]))
+        break
+      case 5: // 23.585 - 23.85
+        getSegments(23.585, 23.85, true).forEach((segment) => t.strictSame(segment, sliceB[i]))
+
+        t.equals(syncActive.callbackCount, 0)
+        t.strictSame(syncActive.aggregateNodes, new Set())
+        t.strictSame(syncActive.asyncIds, new Set())
+
+        t.equals(asyncPending.callbackCount, 4)
+        t.strictSame(asyncPending.aggregateNodes, new Set(['d', 'e', 'f']))
+        t.strictSame(asyncPending.asyncIds, new Set([9, 10, 11, 12]))
+        break
+      case 6: // 23.85 - 24.115
+        getSegments(23.85, 24.115, true).forEach((segment) => t.strictSame(segment, sliceB[i]))
+
+        t.equals(syncActive.callbackCount, 2)
+        t.strictSame(syncActive.aggregateNodes, new Set(['e']))
+        t.strictSame(syncActive.asyncIds, new Set([10, 11]))
+
+        t.equals(asyncPending.callbackCount, 4)
+        t.strictSame(asyncPending.aggregateNodes, new Set(['d', 'e', 'f']))
+        t.strictSame(asyncPending.asyncIds, new Set([9, 10, 11, 12]))
+        break
+      case 7: // 24.115 - 24.38
+        getSegments(24.115, 24.38, true).forEach((segment) => t.strictSame(segment, sliceB[i]))
+
+        t.equals(syncActive.callbackCount, 2)
+        t.strictSame(syncActive.aggregateNodes, new Set(['e']))
+        t.strictSame(syncActive.asyncIds, new Set([10, 11]))
+
+        t.equals(asyncPending.callbackCount, 2)
+        t.strictSame(asyncPending.aggregateNodes, new Set(['d', 'f']))
+        t.strictSame(asyncPending.asyncIds, new Set([9, 12]))
+        break
+      case 8: // 24.38 - 24.645
+        getSegments(24.38, 24.645, true).forEach((segment) => t.strictSame(segment, sliceB[i]))
+
+        t.equals(syncActive.callbackCount, 2)
+        t.strictSame(syncActive.aggregateNodes, new Set(['e']))
+        t.strictSame(syncActive.asyncIds, new Set([10, 11]))
+
+        t.equals(asyncPending.callbackCount, 4)
+        t.strictSame(asyncPending.aggregateNodes, new Set(['d', 'e', 'f', 'g']))
+        t.strictSame(asyncPending.asyncIds, new Set([9, 11, 12, 14]))
+        break
+      case 9: // 24.645 - 24.91
+        getSegments(24.645, 24.91, true).forEach((segment) => t.strictSame(segment, sliceB[i]))
+
+        t.equals(syncActive.callbackCount, 1)
+        t.strictSame(syncActive.aggregateNodes, new Set(['e']))
+        t.strictSame(syncActive.asyncIds, new Set([10]))
+
+        t.equals(asyncPending.callbackCount, 4)
+        t.strictSame(asyncPending.aggregateNodes, new Set(['d', 'e', 'f', 'g']))
+        t.strictSame(asyncPending.asyncIds, new Set([9, 11, 12, 14]))
+        break
+      case 10: // 24.91 - 25.175
+        getSegments(24.91, 25.175, true).forEach((segment) => t.strictSame(segment, sliceB[i]))
+
+        t.equals(syncActive.callbackCount, 3)
+        t.strictSame(syncActive.aggregateNodes, new Set(['d', 'e']))
+        t.strictSame(syncActive.asyncIds, new Set([9, 10, 11]))
+
+        t.equals(asyncPending.callbackCount, 5)
+        t.strictSame(asyncPending.aggregateNodes, new Set(['d', 'e', 'f', 'g', 'h']))
+        t.strictSame(asyncPending.asyncIds, new Set([9, 11, 12, 14, 15]))
+        break
+    }
+  }
+
+  t.end()
+})
