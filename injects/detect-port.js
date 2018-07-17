bf63e6d2083b728b68a419a7d60117d1e02be756
Mathias Buus <mathiasbuus@gmail.com>
2018-07-17 09:12:38 -0300

    do not track on-port detection (#212)
    * do not track on-port detection
    
    * comments

--- injects/detect-port.js
+++ injects/detect-port.js
@@ -1,14 +1,19 @@
 const onlisten = require('on-net-listen')
 const fs = require('fs')
 const net = require('net')
+const logger = require('./logger')
 
 onlisten(function (addr) {
+  // we do async activity below and we do not want that to pollute the
+  // analysis. we use the skipThis flag to opt out of collecting stats here.
+  logger.skipThis = true
   this.destroy()
   const port = Buffer.from(addr.port + '')
   fs.writeSync(3, port, 0, port.length)
   signal(3, function () {
     process.emit('beforeExit')
   })
+  logger.skipThis = false
 })
 
 function signal (fd, cb) {
