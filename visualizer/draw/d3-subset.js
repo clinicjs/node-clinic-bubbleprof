ff1f7e9f9614260c4aaae60dee3b89bdef360001
Renée Kooi <renee@kooi.me>
2018-07-23 09:21:29 +0200

    Use subset of d3 modules.

--- visualizer/draw/d3-subset.js
+++ visualizer/draw/d3-subset.js
@@ -1,23 +1,41 @@
 'use strict'
 
-// During development, use entire D3 library for ease of use
-const d3 = require('d3')
+const selection = require('d3-selection')
 
-/* TODO: on launch, refine down to a subset of d3's microlibraries
-
-// Reduce file size by only including the d3 modules that are used
 const d3 = Object.assign(
-  require('d3-selection'),
-  // Assign all chosen microlibraries to d3-selection to preserve d3.event's live binding
-  // see https://github.com/d3/d3/issues/3102
-  require('d3-array'),
-  require('d3-force'),
-  require('d3-hierarchy'),
+  {},
+  // d3.event
+  // d3.select
+  selection,
+  // d3.scaleLinear
+  // d3.scaleTime
   require('d3-scale'),
+  // d3.arc
+  // d3.area
+  // d3.pie
+  // d3.stack
   require('d3-shape'),
-  require('d3-time-format')
+  // d3.axisBottom
+  require('d3-axis'),
+  // d3.easeCubicInOut
+  require('d3-ease'),
+  // d3.format
+  require('d3-format'),
+  // d3.interpolateNumber
+  require('d3-interpolate'),
+  // d3.timeFormat
+  require('d3-time-format'),
+  // d3.timeHour
+  // d3.timeMinute
+  // d3.timeSecond
+  require('d3-time'),
+  // d3.selection().transition()
+  require('d3-transition')
 )
 
-*/
+// This property changes after importing so we fake a live binding.
+Object.defineProperty(d3, 'event', {
+  get () { return selection.event }
+})
 
 module.exports = d3
