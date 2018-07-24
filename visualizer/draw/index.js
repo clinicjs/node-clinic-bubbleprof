c63e101e3219545523019f16186b95201a2da662
AlanSl <alan.slater@nearform.com>
2018-07-24 14:02:08 +0100

    Use CSS feature selectors instead of class

--- visualizer/draw/index.js
+++ visualizer/draw/index.js
@@ -4,12 +4,8 @@ const BubbleprofUI = require('./bubbleprof-ui.js')
 const staticKeyHtml = require('./static-key.js')
 const d3 = require('./d3-subset.js')
 
-const d3Html = d3.select('html')
-
 function drawOuterUI () {
   // Initial DOM drawing that is independent of data
-
-  d3Html.classed('no-position-sticky', !window.CSS.supports('position', 'sticky'))
 
   const sections = ['header', 'node-link', 'side-bar', 'footer']
   const ui = new BubbleprofUI(sections)
@@ -35,6 +31,7 @@ function drawOuterUI () {
     eventHandler: {
       name: 'click',
       func: () => {
+        const d3Html = d3.select('html')
         // Toggle light theme
         d3Html.classed('light-theme', !d3Html.classed('light-theme'))
       }
