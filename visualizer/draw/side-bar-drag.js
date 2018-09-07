ffc8f2743d8330bafae58aee4fcdfc86dde28642
Renée Kooi <renee@kooi.me>
2018-09-07 15:44:10 +0200

    Also resize stack frame panel

--- visualizer/draw/side-bar-drag.js
+++ visualizer/draw/side-bar-drag.js
@@ -50,13 +50,22 @@ class SideBarDrag extends HtmlContent {
   setNodeLinkWidth (percent) {
     const sideBar = this.ui.sections.get('side-bar')
     const nodeLink = this.ui.sections.get('node-link')
+    const footer = this.ui.sections.get('footer')
     // FIXME this is probably a private API. Is this alright or should the sidebar be its own HtmlContent subclass so it can handle this internally?
     const callbacksOverTime = sideBar.content.get('area-chart')
+    const framesButton = footer.collapseControl
+    const framesPanel = footer.d3ContentWrapper
 
     nodeLink.d3Element
       .style('width', `${percent}%`)
       .classed('redraw', true)
-    sideBar.d3Element.style('width', `${100 - percent}%`)
+    framesPanel
+      .style('width', `${percent}%`)
+    sideBar.d3Element
+      .style('width', `${100 - percent}%`)
+    framesButton.d3Element
+      .style('width', `${100 - percent}%`)
+
     // newPercent / defaultPercent
     // maintaining aspect ratio
     callbacksOverTime.chartHeightScale = (100 - percent) / 25
