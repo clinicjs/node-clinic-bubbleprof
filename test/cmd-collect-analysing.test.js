c2fc87398500e0896f63473c802294ee035c8890
Renée Kooi <renee@kooi.me>
2018-12-05 15:33:28 +0100

    copy-paste lint lol

--- test/cmd-collect-analysing.test.js
+++ test/cmd-collect-analysing.test.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const fs = require('fs')
-const path = require('path')
 const { test } = require('tap')
 const rimraf = require('rimraf')
 const ClinicBubbleprof = require('../index.js')
