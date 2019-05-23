519a43b07015593f3cc1689dc26300e66f466003
Renée Kooi <renee@kooi.me>
2019-05-23 14:40:50 +0200

    fix cluster test
    same as https://github.com/nearform/node-clinic-doctor/pull/242

--- test/cmd-no-cluster.script.js
+++ test/cmd-no-cluster.script.js
@@ -1,10 +1,11 @@
+const path = require('path')
 const rimraf = require('rimraf')
 const ClinicBubbleprof = require('../index.js')
 
 const bubble = new ClinicBubbleprof({})
 bubble.collect([
   process.execPath,
-  '-e', 'var c = require("cluster"); c.isMaster && c.fork()'
+  path.join(__dirname, 'cmd-no-cluster.cluster.js')
 ], (err, result) => {
   rimraf.sync(result)
   if (err) throw err
