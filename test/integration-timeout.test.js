eb4dddc2e1967bca634c4b383ecac7c4a363a7d8
Renée Kooi <renee@kooi.me>
2020-02-24 14:17:24 +0100

    Fix tests on Node 12.16 (#349)
    * Fix expected async operations on node 12.16.0
    
    * fixup! Fix expected async operations on node 12.16.0
    
    * add back PR link for TIMERWRAP
    
    * remove useless comment

--- test/integration-timeout.test.js
+++ test/integration-timeout.test.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const test = require('tap').test
+const semver = require('semver')
 const endpoint = require('endpoint')
 const CollectAndRead = require('./collect-and-read.js')
 const analysis = require('../analysis/index.js')
@@ -21,11 +22,18 @@ test('collect-analysis pipeline', function (t) {
         const nodeMap = new Map(
           aggregateNodes.map((node) => [node.aggregateId, node])
         )
-        t.strictEqual(nodes.length, 2)
-        t.strictEqual(nodeMap.size, 2)
+        if (semver.satisfies(process.version, '>= 12.16.0 < 13.0.0')) {
+          t.strictEqual(nodes.length, 3)
+          t.strictEqual(nodeMap.size, 3)
+          // aggregateId = 1 is the root and points to the Timeout and a PROMISE from Node.js's initialization code
+          t.strictDeepEqual(nodeMap.get(1).children, [2, 3])
+        } else {
+          t.strictEqual(nodes.length, 2)
+          t.strictEqual(nodeMap.size, 2)
+          // aggregateId = 1 is the root and points to the Timeout
+          t.strictDeepEqual(nodeMap.get(1).children, [2])
+        }
 
-        // aggregateId = 1 is the root and points to the Timeout
-        t.strictDeepEqual(nodeMap.get(1).children, [2])
         // aggregateId = 2 is the Timeout
         t.strictEqual(nodeMap.get(2).sources[0].type, 'Timeout')
 
